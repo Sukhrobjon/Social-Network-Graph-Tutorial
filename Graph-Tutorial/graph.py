b75e25828e1913d216a4a9ea3dc0596b15787f69
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-07-27 16:53:08 -0700

    [modified] file structure for better modularity

--- Graph-Tutorial/graph.py
+++ Graph-Tutorial/graph.py
@@ -186,35 +186,3 @@ class Graph:
         # if there is no path from source to destination return -1
         return [], -1
 
-
-# Driver code
-
-
-if __name__ == "__main__":
-
-    # Challenge 1: Create the graph
-
-    g = Graph()
-
-    # Add your friends
-    g.add_vertex("Friend 1")
-    g.add_vertex("Friend 2")
-    g.add_vertex("Friend 3")
-
-    # ...  add all 10 including you ...
-
-    # Add connections (non weighted edges for now)
-    g.add_edge("Friend 1", "Friend 2")
-    g.add_edge("Friend 2", "Friend 3")
-
-    # Challenge 1: Output the vertices & edges
-    # Print vertices
-    print("The vertices are: ", g.get_vertices(), "\n")
-
-    # Print edges
-    print("The edges are: ")
-    for v in g:
-        for w in v.get_neighbors():
-            print(f'({v.get_id()}, {w.get_id()}, {v.get_edge_weight(w)})')
-
-    print("edges: ", g.get_all_edges())
---
+++ Graph-Tutorial/run.py
@@ -0,0 +1,30 @@
+from graph import Graph, Vertex
+
+if __name__ == "__main__":
+
+    # Challenge 1: Create the graph
+
+    g = Graph()
+
+    # Add your friends
+    g.add_vertex("Friend 1")
+    g.add_vertex("Friend 2")
+    g.add_vertex("Friend 3")
+
+    # ...  add all 10 including you ...
+
+    # Add connections (non weighted edges for now)
+    g.add_edge("Friend 1", "Friend 2")
+    g.add_edge("Friend 2", "Friend 3")
+
+    # Challenge 1: Output the vertices & edges
+    # Print vertices
+    print("The vertices are: ", g.get_vertices(), "\n")
+
+    # Print edges
+    print("The edges are: ")
+    for v in g:
+        for w in v.get_neighbors():
+            print(f'({v.get_id()}, {w.get_id()}, {v.get_edge_weight(w)})')
+
+    print("edges: ", g.num_edges)
